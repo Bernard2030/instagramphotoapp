21392285ccdd5a97ec8b0b305a428c2042bc863a
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-14 21:54:57 +0300

    add base html

--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http  import HttpResponse
+
+
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to  InstagramPhotos')
+    return render(request, 'welcome.html')
