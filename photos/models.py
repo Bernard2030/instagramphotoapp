8ae32060a5ea5e378ece9458e2e1a11b26831556
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-15 23:11:46 +0300

    comment class comment

--- photos/models.py
+++ photos/models.py
@@ -6,7 +6,7 @@ from django.db.models.signals import post_save
 
 # Create your models here.
 class Profile(models.Model):
-    photo =models.ImageField(upload_to='photo/', default='default.png')  
+    profile_image =models.ImageField(upload_to='profile_image/', default='default.png')  
     bio = models.TextField(max_length=500, default='My Bio', blank=True) 
     name = models.CharField(blank=True, max_length=120)
     user= models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
@@ -20,7 +20,7 @@ class Profile(models.Model):
     def save_user_profile(sender, instance,created, **kwargs):
         if created:
             Profile.object.create(user=instance)
-            
+
     @receiver(post_save, sender=User) 
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
@@ -38,12 +38,12 @@ class Profile(models.Model):
           
 
 
-class Image(models.Model):
-    image = models.ImageField(upload_to='image/')
+class Post(models.Model):
+    images = models.ImageField(upload_to='images/')
     name =models.CharField(max_length=60, blank=True)
     caption = models.CharField(max_length=250, blank=True)
     likes = models.ManyToManyField(User, related_name='likes', blank=True)
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='image')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='images')
     comments = models.CharField(max_length=200)
 
     class Meta:
@@ -52,29 +52,29 @@ class Image(models.Model):
     @property
     def get_all_comments(self):
         return self.comments.all()
-    def save_image(self):
+    def save_images(self):
         self.save()
 
-    def delete_image(self):
+    def delete_images(self):
         self.delete()
 
     def total_likes(self):
         return self.like.count()
 
     def __str__(self):
-        return f'{self.user.name}Image'                    
+        return f'{self.user.name}Post'                    
 
 
  
 
-class Comment(models.Model):
-    comment = models.TextField()
-    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='comments') 
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments') 
+# class Comment(models.Model):
+#     comment = models.TextField()
+#     images = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments') 
+#     user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments') 
 
-    def __str__(self):
-        return f'{self.user.name} Comment'
+#     def __str__(self):
+#         return f'{self.user.name} Comment'
 
-    class Meta:
-        ordering = ["-pk"]     
+#     class Meta:
+#         ordering = ["-pk"]     
 
