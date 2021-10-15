55f9657aac3af35f316ab25e6abd0cf983105bb3
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-15 23:32:16 +0300

    add test instances

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Post,Profile
 
 # Create your tests here.
+
+#set up method
+def setUp(self):
+    self.bernard = Profile(name = "bernard", user = "moringa")
+
+#Testing Instance
+def test_instance(self):
+    self.assertTrue(isinstance(self.bernard, Profile))    
