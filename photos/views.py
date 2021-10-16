78b0b67d81e718f1f99cd18602a606e29feb6aa7
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-16 22:08:12 +0300

    add login required decorator

--- photos/views.py
+++ photos/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 from photos.models import Post
 
 
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def welcome(request):
     return render(request, 'welcome.html')
 
