473780e22b1e2e6fa7161032cc4a3353afbb1f3d
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-17 22:02:31 +0300

    add url for profile

--- photos/urls.py
+++ photos/urls.py
@@ -7,6 +7,7 @@ urlpatterns=[
     # url('^$',views.welcome,name = 'welcome'),
     url('^$',views.index,name = 'welcome'),
     url(r'^newpost/$',views.new_post,name='newpost'),
+    url(r'^accounts/profile/$',views.profile,name='profile'),
     # url(r'^search/', views.search_results, name='search_results'),
     # url(r'^post/(\d+)',views.article,name ='post'),
     
