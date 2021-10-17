3553d0d927451ad26170716c0959d4c7e60b2718
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-18 00:51:54 +0300

    modify urls in photos

--- photos/urls.py
+++ photos/urls.py
@@ -9,6 +9,11 @@ urlpatterns=[
     url(r'^newpost/$',views.new_post,name='newpost'),
     url(r'^accounts/profile/$',views.profile,name='profile'),
      url(r'^editprofile/$',views.edit_profile,name='editprofile'),
+      url('comment/<int:id>/',views.comment,name='comment'),
+      url(r'^logout/$',views.logout_request,name="logout"),
+      url('post/<int:post_id>/like', views.add_like, name='timeline'),
+      url(r'^search/',views.search_results,name = 'search_results'),
+       url(r'^singlepic/(\d+)',views.single_pic,name='singlepic'),
     # url(r'^search/', views.search_results, name='search_results'),
     # url(r'^post/(\d+)',views.article,name ='post'),
     
